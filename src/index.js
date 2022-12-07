fc8295c1b9cc7610704bd9e223eb9d9e36a921cf
yusuf Demir <yusuf.ferrum@gmail.com>
2022-12-07 12:14:25 +0100

    basename deleted

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL || ""}>
+  <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
