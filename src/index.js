c10b977998510899cfab9ee3f04b189378d4062f
yusuf Demir <yusuf.ferrum@gmail.com>
2022-12-07 12:12:08 +0100

    basename test

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL || ""}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
